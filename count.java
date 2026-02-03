8334994efb1d443d629c836942b14937acd358cd
Gowthamsundar2203 <gowthamsundar2203@gmail.com>
2026-02-03 22:33:30 +0530

    Bsic java

--- count.java
+++ count.java
@@ -1,3 +1,17 @@
 public class count {
-    
+    public static void main(String[] args) {
+        int n = 333;
+        int count = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == 3) {
+                count++;
+            }
+            n = n / 10;
+        }
+
+        System.out.println("Total: " + count);
+        System.out.println(n);
+    }
 }
